9f7a2e9d856bd943953bbcf3812898359197068d
DmitryMustk <dmutrymust@gmail.com>
2023-12-05 08:45:17 +0700

    fix bug with .h/.hpp files

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def is_c_or_cpp_file(file_path):
     return file_extension.lower() in ['.c', '.cpp', '.h', '.hpp']
 
 def process_file(file_path):
+    important_lines = []
     user_includes = []  
     stdlib_includes = []  
     other_code_lines = [] 
@@ -14,7 +15,9 @@ def process_file(file_path):
         lines = file.readlines()
 
     for line in lines:
-        if line.startswith("#include"):
+        if any([line.startswith(i) for i in ["#pragma", "#define", "#ifndef"]]):
+            important_lines.append(line)
+        elif line.startswith("#include"):
             include_path = line.split()[1].strip('"<>')
             if line.startswith("#include \""):
                 user_includes.append(include_path)
@@ -27,6 +30,8 @@ def process_file(file_path):
     stdlib_includes.sort()
 
     with open(file_path, 'w') as file:
+        for line in important_lines:
+            file.write(line)
         for include in user_includes:
             file.write(f'#include "{include}"\n')
         if user_includes:
